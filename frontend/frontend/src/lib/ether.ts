add4e5502a568d87114f1d2484501e86c0addf18
Alan Li <44342646+AlanLiCodes@users.noreply.github.com>
2025-04-26 09:22:56 -0700

    Delete frontend directory

--- frontend/frontend/src/lib/ether.ts
+++
@@ -1,8 +0,0 @@
-import { ethers } from 'ethers';
-import MarketplaceABI from '../deployments/GiftCardMarketplace.json';
-
-const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS!;
-
-export const getContract = (providerOrSigner: ethers.providers.Provider | ethers.Signer) => {
-  return new ethers.Contract(CONTRACT_ADDRESS, MarketplaceABI, providerOrSigner);
-};

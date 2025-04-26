7974151e05c75d0ab07e61c2ee039a5958ef70bc
kent3245 <1126kentziti@gmail.com>
2025-04-25 22:05:51 -0700

    fdsf

--- backend/models/user.py
+++ backend/models/user.py
@@ -7,3 +7,8 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+
+
+
+if __name__ == "__main__":
+    pass

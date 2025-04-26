633a64b88d54c74e8e8e07c88868fc62ccf1e30e
kent3245 <1126kentziti@gmail.com>
2025-04-25 22:56:52 -0700

    initial

---
+++ backend/app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from CardSphereLaHacks.backend.app.utils.database import engine
+from app import models
+
+models.Base.metadata.create_all(bind=engine)  # Creates tables if not exists
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
